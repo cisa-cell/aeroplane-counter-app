71f11055132856bfb14b782f91e2eb794e745989
cisa-cell <cinolsamson2006@gmail.com>
2024-10-12 11:01:06 +0530

    Add files via upload
    cannot figure out css yet.

---
+++ script.js
@@ -0,0 +1,10 @@
+let countEl=document.getElementById('count-el')
+let saveEl=document.getElementById('save-el')
+let count=0
+function increment(){
+  count++
+  countEl.textContent=count
+}
+function save(){
+  saveEl.textContent+=count+"-         "
+}
